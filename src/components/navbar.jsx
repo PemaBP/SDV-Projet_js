3b69cf2d83f1f6f79e62ac7f51e2ae1d94bf1a73
PemaBP <pemabeliseperreard@gmail.com>
2024-03-25 23:07:45 +0100

    navbar sans logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import Link from "next/link";
-import logo from "@/img/logo.png"
+import Logo from "../components/logo.png"
 function navbar() {
   return (
-    <div>Navigation</div>
+    <nav className="bg-back-color-navbar fixed top-8 left-0 right-0 px-10 pt-5 pb-5 flex justify-between items-center " style={{ borderRadius: '15px' }}>
+      <Link href="/" className="flex group justify-start items-center w-1/3 ">
+        <img src={Logo} className="h14 w-auto" alt="Logo" />
+        <span className="text-3xl text-blue-color">PlaceSpotter</span>
+      </Link>
+      <div className="space-x-8 mr-3 text-blue-color "> 
+        <Link href="/" className="text-2xl focus:underline " >Accueil</Link>
+        {" "}
+        <Link href="/add" className="text-2xl focus:underline ">Ajout</Link>
+      </div>
+
+    </nav>
   );
 }
 

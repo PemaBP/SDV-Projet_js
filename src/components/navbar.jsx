3dbeb5bb6db6e6f05c99dcb78bd031068d2149db
PemaBP <pemabeliseperreard@gmail.com>
2024-03-25 13:52:48 +0100

    commit un

---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+import logo from "@/img/logo.png"
+function navbar() {
+  return (
+    <div>Navigation</div>
+  );
+}
+
+export default navbar;
